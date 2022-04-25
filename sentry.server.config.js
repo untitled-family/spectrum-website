2ef52d67f21e5192d37ca9747a5cf0f15b2096cf
Max Bonhomme <hello.bonhomme@gmail.com>
2022-04-25 08:19:37 +0100

    update env var

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://a016e0e4cb7f4600b67bfce908777ea1@o1218193.ingest.sentry.io/6360156',
+  dsn:
+    SENTRY_DSN ||
+    'https://a016e0e4cb7f4600b67bfce908777ea1@o1218193.ingest.sentry.io/6360156',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
