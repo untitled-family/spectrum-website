0090429cb43470020da52980881764d7fb2de4bc
Max Bonhomme <hello.bonhomme@gmail.com>
2022-04-25 11:21:46 +0100

    new contract

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
   infuraId: 'ca44ce9a485946dc9625c88bd5431eed',
-  contractAddress: '0xc07617c76526297082acC326762B7543183B4179',
+  contractAddress: '0xE1B1847CD094504777A3750b4B01de414A310e45',
   price: 0.003,
   friendPrice: 0.002,
 };
