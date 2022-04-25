0fe666075809e443c792a4a9c93c32aea31b6a85
Max Bonhomme <hello.bonhomme@gmail.com>
2022-04-25 11:17:38 +0100

    new address

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -10,6 +10,7 @@ const handler = async (req, res) => {
       const whitelistAddresses = [
         '0xFF5FE6e0D3D48c90A66217dd4A7560a3ed8dACD2',
         '0x9cdd6e35ee8cd6d8f37c19b9cd59395efa330cf1',
+        '0xFc5f56d9957789CB4c3FBFa855cDe02121035567',
       ];
 
       const leafNodes = whitelistAddresses.map((a) => keccak256(a));
