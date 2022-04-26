2bba2d564b745fef9eae8601ba842464e33f17a9
Max Bonhomme <hello.bonhomme@gmail.com>
2022-04-26 08:35:16 +0100

    prod contract

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 export const config = {
   infuraId: 'ca44ce9a485946dc9625c88bd5431eed',
-  contractAddress: '0x122815f8bF3cd28A37234AB522EE6Cf3863997B4',
-  price: 0.003,
-  friendPrice: 0.002,
+  contractAddress: '0x2Ac9795Ca8fc6d09a5748CB9E8B9D67f4B09df07',
+  price: 0.03,
+  friendPrice: 0.02,
 };
