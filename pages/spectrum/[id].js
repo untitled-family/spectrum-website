f124dcb1fbd669487a925e261520934f1d336015
Max Bonhomme <hello.bonhomme@gmail.com>
2022-05-10 15:19:02 +0200

    get layers

--- pages/spectrum/[id].js
+++ pages/spectrum/[id].js
@@ -40,6 +40,27 @@ const Spectrum = () => {
     return detail.toLocaleLowerCase();
   };
 
+  const getLayers = (attributes) => {
+    const _c = attributes.filter((a) => a.trait_type === 'Layer Color');
+    const _s = attributes.filter((a) => a.trait_type === 'Layer Speed');
+    const layers = [];
+
+    _c.forEach((c, index) => {
+      layers.push({
+        color: `rgb(${c.value})`,
+        speed: _s[index].value,
+      });
+    });
+
+    return layers;
+  };
+
+  console.log(data);
+
+  if (data?.spectrum?.metadata?.attributes) {
+    console.log(getLayers(data.spectrum.metadata.attributes));
+  }
+
   return (
     <Box textAlign="center" fontSize="md">
       <Head>
