bf9e7163637485ac3d083251dbff5e3b39a2ef1e
Max Bonhomme <hello.bonhomme@gmail.com>
2022-06-01 11:43:20 +0100

    pagination done

--- pages/spectrum/[id].js
+++ pages/spectrum/[id].js
@@ -11,11 +11,14 @@ import {
 } from '@chakra-ui/react';
 import Head from 'next/head';
 import useSwr from 'swr';
+import { useContractRead } from 'wagmi';
 import { Grid } from '../../components/Grid';
 import { Detail } from '../../components/Detail';
 import { SpectrumSvg } from '../../components/SpectrumSvg';
 import { getDetail, getLayers } from '../../utils/spectrum';
 import { SpectrumExportModal } from '../../components/SpectrumExportModal';
+import { config } from '../../utils/config';
+import contractABI from '../../utils/contractABI.json';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -24,17 +27,32 @@ const Spectrum = () => {
   const { id } = router.query;
   const tokenId = parseInt(id, 10);
   const { data, error } = useSwr(id ? `/api/spectrum/${id}` : null, fetcher);
+  const [{ data: totalSupply }] = useContractRead(
+    {
+      addressOrName: config.contractAddress,
+      contractInterface: contractABI,
+    },
+    'totalSupply'
+  );
 
   const goToPrev = () => {
+    if (!totalSupply._hex) return;
+
+    const total = totalSupply.toNumber();
+
     if (tokenId === 0) {
-      router.push(`/spectrum/${148}`);
+      router.push(`/spectrum/${total - 1}`);
     } else {
       router.push(`/spectrum/${tokenId - 1}`);
     }
   };
 
   const goToNext = () => {
-    if (tokenId >= 148) {
+    if (!totalSupply._hex) return;
+
+    const total = totalSupply.toNumber();
+
+    if (tokenId >= total - 1) {
       router.push(`/spectrum/0`);
     } else {
       router.push(`/spectrum/${tokenId + 1}`);
@@ -47,22 +65,28 @@ const Spectrum = () => {
         <title>Kinetic Spectrums</title>
       </Head>
       <Box alignItems="center" px={6}>
-        <Flex alignItems="center" justifyContent="space-between">
-          <Button
-            height="32px"
-            px={4}
-            lineHeight={1}
-            fontSize="sm"
-            borderRadius="lg"
-            colorScheme="black"
-            border="1px solid rgba(255,255,255,0.2)"
-            _hover={{ borderColor: 'rgba(255,255,255,0.5)' }}
-            background="transparent"
-            minW="80px"
-            onClick={goToPrev}
-          >
-            Previous
-          </Button>
+        <Flex
+          alignItems="center"
+          justifyContent={totalSupply ? 'space-between' : 'center'}
+        >
+          {totalSupply && (
+            <Button
+              height="32px"
+              px={4}
+              lineHeight={1}
+              fontSize="sm"
+              borderRadius="lg"
+              colorScheme="black"
+              border="1px solid rgba(255,255,255,0.2)"
+              _hover={{ borderColor: 'rgba(255,255,255,0.5)' }}
+              background="transparent"
+              minW="80px"
+              onClick={goToPrev}
+            >
+              Previous
+            </Button>
+          )}
+
           <Link href="/">
             <a>
               <Heading
@@ -76,21 +100,23 @@ const Spectrum = () => {
               </Heading>
             </a>
           </Link>
-          <Button
-            minW="80px"
-            height="32px"
-            px={4}
-            lineHeight={1}
-            fontSize="sm"
-            borderRadius="lg"
-            colorScheme="black"
-            border="1px solid rgba(255,255,255,0.2)"
-            _hover={{ borderColor: 'rgba(255,255,255,0.5)' }}
-            background="transparent"
-            onClick={goToNext}
-          >
-            Next
-          </Button>
+          {totalSupply && (
+            <Button
+              minW="80px"
+              height="32px"
+              px={4}
+              lineHeight={1}
+              fontSize="sm"
+              borderRadius="lg"
+              colorScheme="black"
+              border="1px solid rgba(255,255,255,0.2)"
+              _hover={{ borderColor: 'rgba(255,255,255,0.5)' }}
+              background="transparent"
+              onClick={goToNext}
+            >
+              Next
+            </Button>
+          )}
         </Flex>
 
         {data && data?.spectrum?.metadata?.image && (
