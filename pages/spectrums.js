92b4bb570f6df41749e399024b5b783d23b2c2cc
Max Bonhomme <hello.bonhomme@gmail.com>
2022-06-01 22:10:37 +0100

    lil cleanup - shorten owner address - add aspect ratio

--- pages/spectrums.js
+++ pages/spectrums.js
@@ -1,10 +1,18 @@
-import { AspectRatio, Box, Button, Image, SimpleGrid } from '@chakra-ui/react';
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Image,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
 import { gql } from '@apollo/client';
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import client from '../apollo-client';
 import { config } from '../utils/config';
+import { trimAddress } from '../utils/address';
 
 const PAGE_LIMIT = 24;
 
@@ -65,10 +73,6 @@ export default function Spectrums({ mints, pageInfo }) {
   const [spectrums, setSpectrums] = useState(mints);
   const [isLoading, setLoading] = useState(false);
 
-  console.log('mints', mints);
-  console.log('pageInfo', pageInfo);
-  console.log('cursor', cursor);
-
   useEffect(() => {
     setCursor(pageInfo.endCursor);
   }, [pageInfo.endCursor]);
@@ -86,8 +90,8 @@ export default function Spectrums({ mints, pageInfo }) {
   return (
     <Box textAlign="center" fontSize="md">
       <SimpleGrid maxWidth={1600} mx="auto" columns={[1, 1, 2, 2, 3, 3]}>
-        {spectrums.map(({ token }) => (
-          <Link href={`/spectrum/${token.tokenId}`}>
+        {spectrums.map(({ token }, index) => (
+          <Link key={index} href={`/spectrum/${token.tokenId}`}>
             <a>
               <Box p={[6, 8, 8, 8, 12]}>
                 <AspectRatio ratio={1 / 1}>
@@ -97,15 +101,28 @@ export default function Spectrums({ mints, pageInfo }) {
                   />
                 </AspectRatio>
 
-                {token.tokenId}
-                <Box>Owner: {token.owner}</Box>
+                <Text mt={4}>Spectrum #{token.tokenId}</Text>
+                <Text fontWeight="normal">
+                  Owner: {trimAddress(token.owner)}
+                </Text>
               </Box>
             </a>
           </Link>
         ))}
       </SimpleGrid>
       {hasNextPage && (
-        <Button isLoading={isLoading} onClick={handleLoadMore}>
+        <Button
+          mt={4}
+          height="32px"
+          px={4}
+          lineHeight={1}
+          fontSize="sm"
+          borderRadius="lg"
+          colorScheme="white"
+          color="black"
+          isLoading={isLoading}
+          onClick={handleLoadMore}
+        >
           Load more
         </Button>
       )}
